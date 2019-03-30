c92394d01a11081fa493ad9d5c21b75fdae6239d
slavikse <gslavikse@gmail.com>
2019-03-30 19:16:27 +0300

    Удалено всё, более не используемое.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,7 @@
 import Vue from 'vue';
 
-// import 'aframe';
-// import 'aframe-extras';
-// import 'aframe-physics-system';
-
 // import App from './Car.vue';
-// import App from './Maze.vue';
 import App from './Square.vue';
-// import App from './TF_1.vue';
 
 Vue.config.productionTip = false;
 Vue.config.silent = process.env.NODE_ENV === 'production';
