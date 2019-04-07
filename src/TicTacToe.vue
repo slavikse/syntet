49119aa1caf670ffd505702e235736ef17d86399
slavikse <gslavikse@gmail.com>
2019-04-07 23:55:33 +0300

    Генеративный алгоритм.

--- src/TicTacToe.vue
+++ src/TicTacToe.vue
@@ -4,12 +4,15 @@
       Игр: {{ games }} | Побед: {{ victories }}
     </div>
 
-    <div class="field">
+    <div
+      v-for="({ field }, agentIndex) in agents"
+      :key="agentIndex"
+      class="field"
+    >
       <div
-        v-for="(sign, index) in field"
-        :key="index"
+        v-for="(sign, signIndex) in field"
+        :key="`${agentIndex}:${signIndex}`"
         class="cell"
-        @click="fill(index)"
       >
         <div
           v-if="sign === 1"
@@ -26,6 +29,7 @@
 // todo 1 Этап. Обучить сеть играть.
 // todo 2 Этап. Создание площадки для соревнования сетей.
 // todo 3 Этап. Сохранение весов лучшей предобученной сети для игры с человеком.
+
 import * as tf from '@tensorflow/tfjs';
 import { victoryCheckGroups } from './utils';
 
@@ -42,25 +46,41 @@ export default {
       },
       games: 0,
       victories: 0,
-      agent: {
-        id: 1,
-        style: undefined,
-      },
 
-      madeMoves: 0,
-      field: [
-        0, 0, 0,
-        0, 0, 0,
-        0, 0, 0,
+      // todo в цикл
+      agents: [
+        {
+          id: 0,
+          field: [
+            0, 0, 0,
+            0, 0, 0,
+            0, 0, 0,
+          ],
+        },
+        {
+          id: 1,
+          field: [
+            0, 0, 0,
+            0, 0, 0,
+            0, 0, 0,
+          ],
+        },
+        {
+          id: 2,
+          field: [
+            0, 0, 0,
+            0, 0, 0,
+            0, 0, 0,
+          ],
+        },
       ],
-      fieldSize: -1,
+      currentStep: 0,
+      fieldSize: 9,
     };
   },
 
   async mounted() {
     this.setupModel();
-    this.fieldSetting();
-
     await this.modelPredict();
   },
 
@@ -93,43 +113,42 @@ export default {
       });
     },
 
-    fieldSetting() {
-      this.fieldSize = this.field.length;
-    },
-
     async modelPredict() {
-      let isVictories = false;
+      let isModelFit = false;
 
-      const prediction = await this.model.predict(tf.tensor2d([this.field])).data();
-      const cellIndex = this.getCellIndex(prediction);
+      await Promise.all(this.agents.map(async (agent) => {
+        const [prediction] = await this.model.predict(tf.tensor2d([agent.field])).data();
+        const cellIndex = this.getCellIndex(prediction);
 
-      if (this.field[cellIndex] === 0) {
-        this.fill(cellIndex);
+        if (agent.field[cellIndex] === 0) {
+          agent.field.splice(cellIndex, 1, 1);
 
-        // sign: 1 - Это крестик. X.
-        const label = victoryCheckGroups.horizontalGroup({ cells: this.field, sign: 1 })
-          || victoryCheckGroups.verticalGroup({ cells: this.field, sign: 1, start: 0, step: 2 })
-          || victoryCheckGroups.obliquelyGroup({ cells: this.field, sign: 1 });
+          // sign: 1 - Это крестик. X.
+          const label = victoryCheckGroups.horizontalGroup({ cells: agent.field, sign: 1 })
+            || victoryCheckGroups.verticalGroup({ cells: agent.field, sign: 1, start: 0, step: 2 })
+            || victoryCheckGroups.obliquelyGroup({ cells: agent.field, sign: 1 });
 
-        if (label) {
-          isVictories = true;
-          this.victories += 1;
-          this.saveTraining({ label: 1 });
-          await this.modelFit();
+          if (label) {
+            this.victories += 1;
+            this.saveTraining({ field: agent.field, label: [1] });
+
+            isModelFit = true;
+          } else {
+            this.saveTraining({ field: agent.field, label: [0.3] });
+          }
         } else {
-          // todo 0.5 ???
-          this.saveTraining({ label: 0.5 });
+          this.saveTraining({ field: agent.field, label: [0] });
         }
-      } else {
-        this.saveTraining({ label: 0 });
+      }));
+
+      this.currentStep += 1;
+
+      if (this.currentStep === this.agents.length) {
+        isModelFit = true;
       }
 
-      if (!isVictories) {
-        this.madeMoves += 1;
-
-        if (this.madeMoves === this.fieldSize) {
-          await this.modelFit();
-        }
+      if (isModelFit) {
+        await this.modelFit();
       }
 
       // Замедление прогноза для каждого актёра.
@@ -142,17 +161,13 @@ export default {
       await this.modelPredict();
     },
 
-    getCellIndex([prediction]) {
-      return Math.round(prediction * this.field.length);
+    getCellIndex(prediction) {
+      return Math.round(prediction * this.fieldSize);
     },
 
-    fill(index) {
-      this.field.splice(index, 1, 1);
-    },
-
-    saveTraining({ label }) {
-      this.learning.inputs.push(this.field);
-      this.learning.labels.push([label]);
+    saveTraining({ field, label }) {
+      this.learning.inputs.push(field);
+      this.learning.labels.push(label);
     },
 
     async modelFit() {
@@ -166,24 +181,24 @@ export default {
     },
 
     agentReset() {
-      this.madeMoves = 0;
-      this.field = [
-        0, 0, 0,
-        0, 0, 0,
-        0, 0, 0,
-      ];
+      this.agents = this.agents.map((agent, index) => ({
+        id: index,
+        field: [
+          0, 0, 0,
+          0, 0, 0,
+          0, 0, 0,
+        ],
+      }));
+      this.currentStep = 0;
     },
   },
 };
 </script>
 
 <style scoped>
+/* todo расположение нескольких блоков с полями. */
 .TicTacToe {
-  --quantity-rows: 3;
-  --quantity-columns: 3;
-  --square-size: 5rem;
-
-  display: flex;
+  display: grid;
   justify-content: center;
   align-items: center;
   height: 100vh;
@@ -192,7 +207,7 @@ export default {
 
 .stat {
   position: absolute;
-  top: 1rem;
+  top: 0.2rem;
   width: 100%;
   text-align: center;
   font-size: 1.2rem;
@@ -200,6 +215,10 @@ export default {
 }
 
 .field {
+  --quantity-rows: 3;
+  --quantity-columns: 3;
+  --square-size: 5rem;
+
   display: grid;
   grid-template-rows: repeat(var(--quantity-rows), var(--square-size));
   grid-template-columns: repeat(var(--quantity-columns), var(--square-size));
