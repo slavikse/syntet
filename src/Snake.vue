e152bf7c4d5b7edae428ce6dae40337081e1d805
slavikse <gslavikse@gmail.com>
2020-06-21 21:15:46 +0300

    Добавлен вывод статистики игры. Теперь яблок на поле может быть больше одного. Переработано управление теперь срабатывает и для кирилицы. Рефакторинг.

--- src/Snake.vue
+++ src/Snake.vue
@@ -1,13 +1,7 @@
 <template>
   <div
     ref="snake"
-    tabindex="-1"
     class="Snake"
-
-    @keyup.w="handleKeys({ name: 'human', side: 'stepTop' })"
-    @keyup.d="handleKeys({ name: 'human', side: 'stepRight' })"
-    @keyup.s="handleKeys({ name: 'human', side: 'stepBottom' })"
-    @keyup.a="handleKeys({ name: 'human', side: 'stepLeft' })"
   >
     <div class="field">
       <div
@@ -23,6 +17,51 @@
         />
       </div>
     </div>
+
+    <div class="actors">
+      <div
+        v-for="actor in Object.values(actors)"
+        :key="actor.name"
+        class="actor"
+      >
+        <div
+          :style="{ color: actor.colorHead }"
+          class="name"
+        >
+          {{ actor.name }}:
+        </div>
+
+        <div
+          v-if="stats[actor.name]"
+          :style="{ color: actor.colorHead }"
+          class="stats"
+        >
+          <div class="stat">
+            <div class="title">
+              Яблок:
+            </div>
+
+            <div>
+              {{ stats[actor.name].maxApples }}
+            </div>
+          </div>
+
+          <div class="stat">
+            <div class="title">
+              Побед:
+            </div>
+
+            <div>
+              {{ stats[actor.name].victories }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="message">
+      Игра против AI начнётся по нажатию: WASD
+    </div>
   </div>
 </template>
 
@@ -31,34 +70,33 @@ import * as tf from '@tensorflow/tfjs';
 import { nanoid } from 'nanoid';
 import cloneDeep from 'clone-deep';
 
-const fieldSize = 25;
+const fieldSize = 40;
 
-const actorOneHeadColor = 'rgba(201, 52, 65, 1.0)';
-const actorOneTailColor = 'rgba(201, 52, 65, 0.5)';
-
-const actorTwoHeadColor = 'rgba(34, 95, 228, 1.0)';
-const actorTwoTailColor = 'rgba(34, 95, 228, 0.5)';
-
-const basicColor = '#222';
 const barrierColor = '#111';
+const basicColor = '#222';
 const appleColor = 'green';
 
-const basicReward = 0.1;
-// const barrierReward = -1;
-// const appleReward = 1;
+const actorsColors = [
+  // human
+  'rgba(201, 52, 65, 1.0)',
+  'rgba(201, 52, 65, 0.5)',
 
-const numberSides = 4;
-const autoMovementDelay = 500;
+  // ai
+  'rgba(34, 95, 228, 1.0)',
+  'rgba(34, 95, 228, 0.5)',
+];
 
-const colors = [
-  actorOneHeadColor,
-  actorOneTailColor,
-
-  actorTwoHeadColor,
-  actorTwoTailColor,
-
+const blocksColors = [
+  ...actorsColors,
   barrierColor,
 ];
+
+const keysWASD = {
+  KeyW: 'stepTop',
+  KeyD: 'stepRight',
+  KeyS: 'stepBottom',
+  KeyA: 'stepLeft',
+};
 
 const sides = {
   0: 'stepTop',
@@ -68,6 +106,15 @@ const sides = {
 };
 
 let nextFrameId = -1;
+
+const basicReward = 0.1;
+// const barrierReward = -1;
+// const appleReward = 1;
+
+const numberSides = 4;
+const autoMovementDelay = 150;
+
+const isDev = true;
 
 export default {
   name: 'Snake',
@@ -82,48 +129,65 @@ export default {
         human: {
           name: 'human',
           side: 'stepBottom',
+
+          colorHead: actorsColors[0],
+          colorTail: actorsColors[1],
+          cells: [
+            { id: nanoid(), position: { y: 5, x: 3 } },
+            { id: nanoid(), position: { y: 4, x: 3 } },
+            { id: nanoid(), position: { y: 3, x: 3 } },
+          ],
+
           model: tf.sequential(),
           training: { inputs: [], labels: [] },
           rewards: Array(numberSides).fill(basicReward),
-          colorHead: colors[0],
-          colorTail: colors[1],
-          cells: [
-            { id: nanoid(), position: { x: 3, y: 5 } },
-            { id: nanoid(), position: { x: 3, y: 4 } },
-            { id: nanoid(), position: { x: 3, y: 3 } },
-          ],
         },
         ai: {
           name: 'ai',
           side: 'stepTop',
+
+          colorHead: actorsColors[2],
+          colorTail: actorsColors[3],
+          cells: [
+            { id: nanoid(), position: { y: fieldSize - 9, x: fieldSize - 4 } },
+            { id: nanoid(), position: { y: fieldSize - 8, x: fieldSize - 4 } },
+            { id: nanoid(), position: { y: fieldSize - 7, x: fieldSize - 4 } },
+            { id: nanoid(), position: { y: fieldSize - 6, x: fieldSize - 4 } },
+            { id: nanoid(), position: { y: fieldSize - 5, x: fieldSize - 4 } },
+            { id: nanoid(), position: { y: fieldSize - 4, x: fieldSize - 4 } },
+          ],
+
           model: tf.sequential(),
           training: { inputs: [], labels: [] },
           rewards: Array(numberSides).fill(basicReward),
-          colorHead: colors[2],
-          colorTail: colors[3],
-          cells: [
-            { id: nanoid(), position: { x: fieldSize - 4, y: fieldSize - 9 } },
-            { id: nanoid(), position: { x: fieldSize - 4, y: fieldSize - 8 } },
-            { id: nanoid(), position: { x: fieldSize - 4, y: fieldSize - 7 } },
-            { id: nanoid(), position: { x: fieldSize - 4, y: fieldSize - 6 } },
-            { id: nanoid(), position: { x: fieldSize - 4, y: fieldSize - 5 } },
-            { id: nanoid(), position: { x: fieldSize - 4, y: fieldSize - 4 } },
-          ],
         },
       },
       actorsDefault: {},
 
-      // todo переключение режимов (mode), чтобы играть против МЛ вручную и потом возобновить обучение.
-      mode: 'gaming', // training | gaming
+      stats: {},
+
+      apples: [
+        { y: -1, x: -1 },
+        { y: -1, x: -1 },
+      ],
+
+      mode: isDev ? 'gaming' : 'training',
     };
   },
 
   mounted() {
     this.initialField();
     this.initialActors();
-    this.addApple();
+    this.initialStats();
 
+    this.apples.forEach(this.addApple);
     this.autoStep();
+
+    window.addEventListener('keydown', this.handleKeys);
+  },
+
+  destroyed() {
+    window.removeEventListener('keydown', this.handleKeys);
   },
 
   methods: {
@@ -151,29 +215,52 @@ export default {
       Object.values(this.actors).forEach(this.setupModel);
       this.actorsDefault = cloneDeep(this.actors);
 
+      this.resetActors();
+    },
+
+    resetActors() {
       Object.values(this.actors).forEach((actor) => {
-        actor.cells.forEach(({ position: { x, y } }, cellIndex) => {
+        actor.cells.forEach(({ position: { y, x } }, cellIndex) => {
           this.field[y][x] = cellIndex === 0 ? actor.colorHead : actor.colorTail;
         });
 
         actor.rewards = actor.rewards.map(() => basicReward);
+
+        this.stats[actor.name] = {
+          maxApples: 0,
+        };
       });
     },
 
-    addApple() {
+    initialStats() {
+      Object.values(this.actors).forEach((actor) => {
+        this.stats[actor.name] = {
+          maxApples: 0,
+          victories: 0,
+        };
+      });
+    },
+
+    addApple(coords) {
       const y = Math.round(Math.random() * (fieldSize - 1));
       const x = Math.round(Math.random() * (fieldSize - 1));
 
       if (this.field[y][x] === basicColor) {
+        coords.y = y;
+        coords.x = x;
+
         this.field[y][x] = appleColor;
       } else {
-        this.addApple();
+        this.addApple(coords);
       }
     },
 
-    handleKeys({ name, side }) {
-      if (this.mode === 'gaming') {
-        this.actors[name].side = side;
+    handleKeys({ code }) {
+      const side = keysWASD[code];
+
+      if (side) {
+        this.mode = 'gaming';
+        this.actors.human.side = side;
       }
     },
 
@@ -184,7 +271,6 @@ export default {
         } else {
           this.estimationSides(actor);
 
-          // 0 - stepTop, 1 - stepRight, 2 - stepBottom, 3 - stepLeft.
           const sideIndex = await this.modelPredict(actor);
           actor.side = sides[sideIndex];
         }
@@ -209,7 +295,7 @@ export default {
     },
 
     clearTail({ cells }) {
-      const { position: { x, y } } = cells[cells.length - 1];
+      const { position: { y, x } } = cells[cells.length - 1];
       this.field[y][x] = basicColor;
     },
 
@@ -218,75 +304,80 @@ export default {
         const cell = actor.cells[i];
         cell.position = actor.cells[i - 1].position;
 
-        const { position: { x, y } } = cell;
+        const { position: { y, x } } = cell;
         this.field[y][x] = actor.colorTail;
       }
     },
 
     stepHead({ actor, side }) {
       const [cell] = actor.cells;
-      const { position: { x, y } } = cell;
+      const { position: { y, x } } = cell;
 
-      this[side]({ cell, x, y });
+      this[side]({ cell, y, x });
 
       this.nextCell(actor);
     },
 
-    stepTop({ cell, x, y }) {
-      cell.position = { x, y: y - 1 };
+    stepTop({ cell, y, x }) {
+      cell.position = { y: y - 1, x };
     },
 
-    stepRight({ cell, x, y }) {
-      cell.position = { x: x + 1, y };
+    stepRight({ cell, y, x }) {
+      cell.position = { y, x: x + 1 };
     },
 
-    stepBottom({ cell, x, y }) {
-      cell.position = { x, y: y + 1 };
+    stepBottom({ cell, y, x }) {
+      cell.position = { y: y + 1, x };
     },
 
-    stepLeft({ cell, x, y }) {
-      cell.position = { x: x - 1, y };
+    stepLeft({ cell, y, x }) {
+      cell.position = { y, x: x - 1 };
     },
 
     nextCell(actor) {
-      const [{ position: { x, y } }] = actor.cells;
+      const [{ position: { y, x } }] = actor.cells;
       const nextColor = this.field[y][x];
 
-      if (colors.includes(nextColor)) {
+      if (blocksColors.includes(nextColor)) {
         this.gameOver(actor);
       } else {
         this.field[y].splice(x, 1, actor.colorHead);
 
         if (nextColor === appleColor) {
-          this.growth({ actor, x, y });
-          this.addApple();
+          this.growth({ actor, y, x });
+
+          const coords = this.apples.find((a) => `${a.y}_${a.x}` === `${y}_${x}`);
+          this.addApple(coords);
         }
       }
     },
 
-    growth({ actor, x, y }) {
+    growth({ actor, y, x }) {
+      this.stats[actor.name].maxApples += 1;
+
       actor.cells.push({
         id: nanoid(),
-        position: { x, y },
+        position: { y, x },
       });
     },
 
-    // todo остановить игру, пока идёт обучение
     gameOver(actor) {
       cancelAnimationFrame(nextFrameId);
       clearTimeout(nextFrameId);
       nextFrameId = -1;
 
-      this.field = cloneDeep(this.fieldDefault);
-      this.actors = cloneDeep(this.actorsDefault);
+      this.modelsFit({
+        actor,
+        callbackAtEndTraining: () => {
+          this.field = cloneDeep(this.fieldDefault);
+          this.actors = cloneDeep(this.actorsDefault);
 
-      this.initialActors();
-      this.addApple();
+          this.resetActors();
+          this.apples.forEach(this.addApple);
 
-      console.log('game over:', actor.name);
-      // todo обучение моделей. fit
-
-      // this.runGame();
+          this.mode = isDev ? 'gaming' : 'training';
+        },
+      });
     },
 
     // ML алгоритм.
@@ -317,13 +408,19 @@ export default {
     // todo врага так же разметить как -1 (стена). отметить всех и себя тоже
     // todo яблоко +1 для стороны
     estimationSides(actor) {
-      console.log(actor);
+      actor.iii = 0;
       // actor.rewards
     },
 
     searchApples() {},
 
+    /**
+     * 0 - stepTop, 1 - stepRight, 2 - stepBottom, 3 - stepLeft.
+     * @param actor
+     * @returns {Promise<Number>}
+     */
     async modelPredict(actor) {
+      // todo
       const predictions = await actor.model.predict(tf.tensor2d([actor.rewards])).data();
       return predictions.indexOf(Math.max(...predictions));
     },
@@ -331,6 +428,13 @@ export default {
     // todo алгоритм оценки текущего положения и куда лучше сходить, после чего, ML выберет ход.
     // actor.rewards[nextColor] = barrierReward;
     // actor.rewards[nextColor] = appleReward;
+
+    // todo обучение моделей. fit
+    // actor,
+    modelsFit({ callbackAtEndTraining }) {
+      // console.log(actor);
+      callbackAtEndTraining();
+    },
   },
 };
 </script>
@@ -342,32 +446,70 @@ export default {
 .Snake {
   --rows: -1;
   --columns: -1;
-  --size: 30px;
+  --size: 25px;
 
   position: relative;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100vh;
   user-select: none;
   outline: none;
-}
 
-.field {
-  display: grid;
-  grid-template-rows: repeat(var(--rows), var(--size));
-
-  .row {
+  .field {
     display: grid;
-    grid-template-columns: repeat(var(--columns), var(--size));
+    grid-template-rows: repeat(var(--rows), var(--size));
 
-    .cell {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      font-size: 12px;
-      color: #555;
+    .row {
+      display: grid;
+      grid-template-columns: repeat(var(--columns), var(--size));
+
+      .cell {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 12px;
+        color: #555;
+      }
     }
+  }
+
+  .actors {
+    margin-top: 20px;
+
+    .actor {
+      margin-bottom: 10px;
+      display: flex;
+      justify-content: space-between;
+
+      .name {
+        padding-right: 10px;
+        width: 100%;
+        text-align: end;
+        text-transform: uppercase;
+      }
+
+      .stats {
+        font-weight: bold;
+
+        .stat {
+          display: flex;
+          justify-content: space-between;
+          min-width: 70px;
+
+          .title {
+            padding-right: 4px;
+          }
+        }
+      }
+    }
+  }
+
+  .message {
+    margin-top: 20px;
+    text-align: center;
+    color: gray;
   }
 }
 </style>
