36affa69ae05328a608e2b5ac12ade46b3a0d55c
slavikse <slavikse@yandex.ru>
2022-04-16 14:20:40 +0300

    deps

--- PYTHON/main.py
+++ PYTHON/main.py
@@ -1,16 +1,10 @@
-''' Задачи:
-Лабиринт:
-+ 1. Статичный
-+ 2. Бесконечный
-+ 3. Генерация: барьеры и путь
-+ 4. Агент (координаты)
-+ 5. Прохождение
-'''
-
 from os import system
-from time import sleep, time
+from time import sleep
 from random import choice
 
+import tensorflow as tf
+import numpy as np
+import operator
 
 free = ' '
 ghost = '*'
@@ -52,6 +46,26 @@ world_block_row_count = len(world_blocks['0:0']) // len(world_block_empty)
 indexes_last_free_cells = [-3, -1]
 agent_state = {'init': False, 'x': -1, 'y': -1, 'step_index': -1}
 
+# ML Configure
+models = tf.keras.models
+layers = tf.keras.layers
+
+inputs = []  # [[]]
+labels = []  # [[]]
+
+model = models.Sequential([
+    layers.Dense(3, activation='relu'),
+    layers.Dropout(0.2),
+    layers.Dense(10, activation='relu'),
+    layers.Dense(3, activation='softmax')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='mse',
+    metrics=['mae']  # mae | accuracy
+)
+
 
 def generate_blocks_to_scene():
     global world
@@ -83,6 +97,7 @@ def draw_scene():
         if cell_index % len(world_block_empty) == 0:
             x = 0
             y += 1
+
             print()
 
         else:
@@ -107,7 +122,7 @@ def draw_scene():
 
 
 def redraw_actor():
-    top_index = can_actor_step_top()
+    top_index = get_actor_step_top()
     step_index = -1
 
     if top_index >= 0 and world[top_index] == free:
@@ -115,14 +130,14 @@ def redraw_actor():
         agent_state['y'] -= 1
 
     else:
-        left_index = can_actor_step_left()
+        left_index = get_actor_step_left()
 
         if world[left_index] == free:
             step_index = left_index
             agent_state['x'] -= 1
 
         else:
-            right_index = can_actor_step_right()
+            right_index = get_actor_step_right()
 
             if world[right_index] == free:
                 step_index = right_index
@@ -131,19 +146,55 @@ def redraw_actor():
     agent_state['step_index'] = step_index
 
 
-def can_actor_step_top():
+def redraw_actor_ml():
+    top_index = get_actor_step_top()
+    left_index = get_actor_step_left()
+    right_index = get_actor_step_right()
+
+    [pred] = model.predict([[top_index, left_index, right_index]])
+    pred_index, pred_value = max(enumerate(pred), key=operator.itemgetter(1))
+    print(pred_index, pred_value)
+
+    # todo проверка пересечения с перегородкой, куда собирается перейти агент
+    # вызывать при попадании в перегородку: model.fit(inputs, labels, epochs=16)
+    if pred_index == 0:
+        agent_state['y'] -= 1
+
+    elif pred_index == 1:
+        agent_state['x'] -= 1
+
+    elif pred_index == 2:
+        agent_state['x'] += 1
+
+    print(agent_state)
+
+    agent_state['step_index'] = pred_index
+
+    # ///
+
+    inputs.append([top_index, left_index, right_index])
+
+    # todo выходит за пределы поля
+    labels.append([
+        1 if top_index >= 0 and world[top_index] == free else 0.1,
+        1 if world[left_index] == free else 0.1,
+        1 if world[right_index] == free else 0.1,
+    ])
+
+
+def get_actor_step_top():
     return agent_state['x'] + (agent_state['y'] - 1) * len(world_block_empty)
 
 
-def can_actor_step_left():
+def get_actor_step_left():
     return (agent_state['x'] - 1) + agent_state['y'] * len(world_block_empty)
 
 
-def can_actor_step_right():
+def get_actor_step_right():
     return (agent_state['x'] + 1) + agent_state['y'] * len(world_block_empty)
 
 
-def clear_scene(delay=0.5):
+def clear_scene(delay=0.1):
     sleep(delay)
     system('clear')
 
@@ -158,12 +209,11 @@ def add_blocks_to_scene():
 
 
 def game_loop():
-    print(agent_state)
-
     clear_scene()
     add_blocks_to_scene()
     draw_scene()
-    redraw_actor()
+    # redraw_actor()
+    redraw_actor_ml()
 
 
 generate_blocks_to_scene()
